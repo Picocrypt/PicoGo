1aef9f5efd8c8a013521cf39d72a19cd3a0b9e33
Nathan <nathan.huffman2016@gmail.com>
2025-04-27 13:17:04 -0400

    Remove state
    Adding the same ui elements to multiple canvases might get weird
    with state tracking. Just create fresh ones as needed, and do not
    maintain references to old ones

--- internal/ui/elements.go
+++ internal/ui/elements.go
@@ -8,7 +8,6 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
@@ -29,36 +28,6 @@ func (u *UpdateMethods) Update() {
 	}
 }
 
-type KeyfileElement struct {
-	text      *widget.Entry
-	addBtn    *widget.Button
-	createBtn *widget.Button
-	clearBtn  *widget.Button
-	state     *State
-}
-
-func (k *KeyfileElement) UpdateText() {
-	names := []string{}
-	for _, kf := range k.state.Keyfiles {
-		names = append(names, kf.Name())
-	}
-	text := strings.Join(names, "\n")
-	if k.text.Text != text {
-		k.text.Text = text
-		k.text.Refresh()
-	}
-}
-
-func (k *KeyfileElement) Container() fyne.CanvasObject {
-	c := container.New(
-		layout.NewVBoxLayout(),
-		keyfileOrderedCheck(k.state),
-		k.text,
-		container.New(layout.NewHBoxLayout(), k.addBtn, k.createBtn, k.clearBtn),
-	)
-	return c
-}
-
 func keyfileText() *widget.Entry {
 	text := widget.NewMultiLineEntry()
 	text.Disable()
@@ -66,101 +35,137 @@ func keyfileText() *widget.Entry {
 	return text
 }
 
-func keyfileAddBtn(state *State, logger *Logger, window fyne.Window) *widget.Button {
+func keyfileAddCallback(state *State, logger *Logger, window fyne.Window, textUpdate func()) func(fyne.URIReadCloser, error) {
+	return func(reader fyne.URIReadCloser, err error) {
+		defer textUpdate()
+		if reader != nil {
+			defer reader.Close()
+		}
+		if err != nil {
+			logger.Log("Adding keyfile failed", *state, err)
+			dialog.ShowError(fmt.Errorf("adding keyfile: %w", err), window)
+			return
+		}
+		if reader != nil {
+			state.AddKeyfile(reader.URI())
+			logger.Log("Adding keyfile complete", *state, nil)
+		} else {
+			logger.Log("Adding keyfile canceled", *state, nil)
+		}
+	}
+}
+
+func keyfileAddBtn(state *State, logger *Logger, window fyne.Window, textUpdate func()) *widget.Button {
 	btn := widget.NewButtonWithIcon("Add", theme.ContentAddIcon(), func() {
-		fd := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-			if reader != nil {
-				defer reader.Close()
-			}
-			if err != nil {
-				logger.Log("Adding keyfile failed", *state, err)
-				dialog.ShowError(fmt.Errorf("adding keyfile: %w", err), window)
-				return
-			}
-			if reader != nil {
-				state.AddKeyfile(reader.URI())
-				logger.Log("Adding keyfile complete", *state, nil)
-			} else {
-				logger.Log("Adding keyfile canceled", *state, nil)
-			}
-		}, window)
+		fd := dialog.NewFileOpen(keyfileAddCallback(state, logger, window, textUpdate), window)
 		fd.Show()
 	})
 	return btn
 }
 
-func keyfileCreateBtn(state *State, logger *Logger, window fyne.Window) *widget.Button {
-	btn := widget.NewButtonWithIcon("Create", theme.ContentAddIcon(), func() {
-		fd := dialog.NewFileSave(func(writer fyne.URIWriteCloser, err error) {
-			if writer != nil {
-				defer writer.Close()
-			}
+func keyfileCreateCallback(state *State, logger *Logger, window fyne.Window, textUpdate func()) func(fyne.URIWriteCloser, error) {
+	return func(writer fyne.URIWriteCloser, err error) {
+		defer textUpdate()
+		if writer != nil {
+			defer writer.Close()
+		}
+		if err != nil {
+			logger.Log("Creating keyfile failed", *state, err)
+			dialog.ShowError(fmt.Errorf("creating keyfile: %w", err), window)
+			return
+		}
+		if writer != nil {
+			data := make([]byte, 32)
+			_, err := rand.Read(data)
 			if err != nil {
-				logger.Log("Creating keyfile failed", *state, err)
+				logger.Log("Creating keyfile data failed", *state, err)
 				dialog.ShowError(fmt.Errorf("creating keyfile: %w", err), window)
 				return
 			}
-			if writer != nil {
-				data := make([]byte, 32)
-				_, err := rand.Read(data)
-				if err != nil {
-					logger.Log("Creating keyfile data failed", *state, err)
-					dialog.ShowError(fmt.Errorf("creating keyfile: %w", err), window)
-					return
-				}
-				_, err = writer.Write(data)
-				if err != nil {
-					logger.Log("Writing keyfile failed", *state, err)
-					dialog.ShowError(fmt.Errorf("writing keyfile: %w", err), window)
-					return
-				}
-				state.AddKeyfile(writer.URI())
-				logger.Log("Created keyfile", *state, nil)
-			} else {
-				logger.Log("Creating keyfile canceled", *state, nil)
+			_, err = writer.Write(data)
+			if err != nil {
+				logger.Log("Writing keyfile failed", *state, err)
+				dialog.ShowError(fmt.Errorf("writing keyfile: %w", err), window)
+				return
 			}
-		}, window)
+			state.AddKeyfile(writer.URI())
+			logger.Log("Created keyfile", *state, nil)
+		} else {
+			logger.Log("Creating keyfile canceled", *state, nil)
+		}
+	}
+}
+
+func keyfileCreateBtn(state *State, logger *Logger, window fyne.Window, textUpdate func()) *widget.Button {
+	btn := widget.NewButtonWithIcon("Create", theme.ContentAddIcon(), func() {
+		fd := dialog.NewFileSave(keyfileCreateCallback(state, logger, window, textUpdate), window)
 		fd.SetFileName("Keyfile")
 		fd.Show()
 	})
 	return btn
 }
 
-func keyfileClearBtn(state *State, logger *Logger) *widget.Button {
-	btn := widget.NewButtonWithIcon("Clear", theme.ContentClearIcon(), func() {
+func keyfileClearCallback(state *State, logger *Logger, textUpdate func()) func() {
+	return func() {
+		defer textUpdate()
+		logger.Log("Clearing keyfiles", *state, nil)
 		state.Keyfiles = state.Keyfiles[:0]
-		logger.Log("Cleared keyfiles", *state, nil)
-	})
+	}
+}
+
+func keyfileClearBtn(state *State, logger *Logger, textUpdate func()) *widget.Button {
+	btn := widget.NewButtonWithIcon("Clear", theme.ContentClearIcon(), keyfileClearCallback(state, logger, textUpdate))
 	return btn
 }
 
+func keyfileOrderedCallback(state *State) func(bool) {
+	return func(checked bool) {
+		(*state).OrderedKeyfiles = checked
+	}
+}
+
 func keyfileOrderedCheck(state *State) *widget.Check {
-	orderedKeyfiles := widget.NewCheckWithData("Require correct order", binding.BindBool(&(*state).OrderedKeyfiles))
+	orderedKeyfiles := widget.NewCheck("Require correct order", keyfileOrderedCallback(state))
+	orderedKeyfiles.SetChecked(state.OrderedKeyfiles)
 	if state.IsDecrypting() {
 		orderedKeyfiles.Disable()
 	}
 	return orderedKeyfiles
 }
 
-func makeKeyfileElement(logger *Logger, state *State, window fyne.Window) *KeyfileElement {
-	element := &KeyfileElement{
-		state:     state,
-		text:      keyfileText(),
-		addBtn:    keyfileAddBtn(state, logger, window),
-		createBtn: keyfileCreateBtn(state, logger, window),
-		clearBtn:  keyfileClearBtn(state, logger),
+func keyfileTextUpdate(state *State, text *widget.Entry) func() {
+	return func() {
+		names := []string{}
+		for _, kf := range state.Keyfiles {
+			names = append(names, kf.Name())
+		}
+		msg := strings.Join(names, "\n")
+		if text.Text != msg {
+			text.Text = msg
+			text.Refresh()
+		}
 	}
-	return element
 }
 
 func MakeKeyfileBtn(logger *Logger, state *State, updates *UpdateMethods, window fyne.Window) *widget.Button {
-	element := makeKeyfileElement(logger, state, window)
 	btn := widget.NewButtonWithIcon("Keyfile", theme.ContentAddIcon(), func() {
-		c := element.Container()
+		text := keyfileText()
+		textUpdate := keyfileTextUpdate(state, text)
+		textUpdate()
+		c := container.New(
+			layout.NewVBoxLayout(),
+			keyfileOrderedCheck(state),
+			text,
+			container.New(
+				layout.NewHBoxLayout(),
+				keyfileAddBtn(state, logger, window, textUpdate),
+				keyfileCreateBtn(state, logger, window, textUpdate),
+				keyfileClearBtn(state, logger, textUpdate),
+			),
+		)
 		dialog.ShowCustom("Keyfiles", "Done", c, window)
 	})
 	updates.Add(func() {
-		element.UpdateText()
 		btnName := "Keyfiles [" + strconv.Itoa(len(state.Keyfiles)) + "]"
 		if btn.Text != btnName {
 			btn.Text = btnName
